1b74ba99473195b15f848fb9382dce833564f58e
JayPrakash <stcstest70@gmail.com>
2023-09-18 17:51:48 +0530

    new

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ import bodyParser from "body-parser";
 //     origin: allowedOrigins,
 //     credentials: true,
 //   }));
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://testapp-tau-inky.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
